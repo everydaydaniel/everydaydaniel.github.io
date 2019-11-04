1c03b6ca51c83ab7e4c4d46da6e07e6bbd354516
System Administrator <root@daniels-mbp.austin.ibm.com>
2019-11-04 11:30:01 -0600

    Updates

---
+++ precache-manifest.b2e814355660de0e5d54051f16d6f615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b6a9c49d74ff05d36eb7481e0e30eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "430233a5e95886a2da2d",
+    "url": "/static/css/main.0853d846.chunk.css"
+  },
+  {
+    "revision": "b1cc2d89d5a22f6a34b1",
+    "url": "/static/js/2.6dc31d5d.chunk.js"
+  },
+  {
+    "revision": "430233a5e95886a2da2d",
+    "url": "/static/js/main.9cfdc77d.chunk.js"
+  },
+  {
+    "revision": "61255ee5d144ec9dd61f",
+    "url": "/static/js/runtime-main.81a050e9.js"
+  }
+]);
--- src/apis/email_token.js
+++
@@ -1,7 +0,0 @@
-const api_keys = {
-  SERVICE_ID: 'goody_bag_emai',
-  USER_ID: 'user_ii2AGhcG0hUukoNlbQ1tO',
-  TEMPLATE_ID: 'template_WBPDRPzX'
-}
-
-export default api_keys;
